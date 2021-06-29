cfe278216bf7cdecb7000ebbd39a73b23aecd7ed
wanghuixi <huixi.wang@orca-corp.com>
2021-06-30 00:55:03 +0800

    测试提交

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"reflect"
+)
+
+func main() {
+	now := time.Now()
+	h, _ := time.ParseDuration("24h")
+	fmt.Println(int32(now.Add(2 * h).Unix()))
+	var num int32  = 23
+	var num1 int32  = 2
+
+	fmt.Println(reflect.TypeOf(num * num1 + int32(3)))	
+
+
+
+
+
+}
