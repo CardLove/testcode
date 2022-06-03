926dcf4e290c994dc0c3b505f4d610319c27a35d
wanghuixi <huixi.wang@orca-corp.com>
2022-06-03 10:06:34 +0800

    生成随机号码

--- main.go
+++ main.go
@@ -1,21 +1,34 @@
 package main
 
-import (
-	"fmt"
-	"reflect"
-	"time"
-)
+import "fmt"
+
+var max int64
+var str []string
 
 func main() {
-	now := time.Now()
-	h, _ := time.ParseDuration("24h")
-	fmt.Println(int32(now.Add(2 * h).Unix()))
-	var num int32 = 23
-	var num1 int32 = 2
+	CreateNum(4)
+	fmt.Println("max", max)
+	fmt.Println("end")
+}
 
-	fmt.Println(reflect.TypeOf(num*num1 + int32(3)))
+func CreateNum(number int32) {
+	seedNumber := []string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
+	//seedNumber := []string{"0", "1", "2", "3", "4", "5"}
 
-	t := time.Now()
-	fmt.Println(int(t.Weekday()))
-
+	ret := []string{}
+	base := seedNumber
+	for j := int32(0); j < number-1; j++ {
+		for _, v := range base {
+			for _, v1 := range seedNumber {
+				newNum := v + v1
+				ret = append(ret, newNum)
+				if int32(len(newNum)) == number {
+					max++
+					//fmt.Println(newNum)
+				}
+			}
+		}
+		base = ret
+	}
+	//fmt.Println(max)
 }
